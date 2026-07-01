62ed28b5ac2d4db23186fc55287ebd143ab6977b
JimeBlue <jimeblau@gmail.com>
2026-07-01 12:22:58 +0200

    add experss and cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 // Initialize the Express application instance
 const app = express();
 
 const port = process.env.PORT || 8080;
+
+// middleware
+app.use(express.json()); // this one is for whenever we need to run json and send it back and forth in our requests
+app.use(cors()); // allow requests from other origins (e.g. a frontend running on a different port)
 
 // app.get--> 2 arguments: where (location) and what (request and response)
 app.get("/", (req, res) => {
